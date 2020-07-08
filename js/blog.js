8fbfeea583d588ee669da3440b14c9598c658cdb
Verta-master <vertyagina@gmail.com>
2020-07-08 14:47:33 +0600

    Catalog

---
+++ js/blog.js
@@ -0,0 +1,36 @@
+//Catalog slider
+var breakpoint2 = window.matchMedia('(min-width: 1200px)');
+var newsSwiper2;
+var breakpointChecker2 = function() {
+  if (breakpoint2.matches === true) {
+    if (newsSwiper2 !== undefined) newsSwiper2.destroy(true, true);
+    return;
+    } else if (breakpoint2.matches === false) {
+      return enableSwiper2();
+    }
+};
+
+var enableSwiper2 = function() {
+  newsSwiper2 = new Swiper ('.blog__slider', {
+    navigation: {
+      nextEl: '.blog__next',
+      prevEl: '.blog__prev',
+    },
+    simulateTouch: false,
+    preventClicks: false,
+    preventClicksPropagation: false,
+    spaceBetween: 30,
+    slidesPerView: 2,
+      breakpoints: {
+      1022: {
+        slidesPerView: 2,
+      },
+      480: {
+        slidesPerView: 1,
+      }
+    }
+  })
+};
+
+breakpoint2.addListener(breakpointChecker2);
+breakpointChecker2();
